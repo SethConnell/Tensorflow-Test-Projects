ae63ec56af441e0d555dddc5b2309f2fad8a0386
Seth J. Connell <sethjordanconnell@gmail.com>
2018-09-06 01:06:48 -0500

    Added Matrix Math to MathTest.py

--- MathTest.py
+++ MathTest.py
@@ -1,10 +1,19 @@
+# Made Imports
 import tensorflow as tf
 
+
+# Basic Math.
 const_a = tf.constant(3.6)
 const_b = tf.constant(1.2)
 total = const_a + const_b
 quot = tf.div(const_a, const_b)
 
+# Matrix Multiplication.
+mat_a = tf.constant([[2,3], [1,2], [4,5]])
+mat_b = tf.constant([[6,4,1], [3,7,2]])
+mat_product = tf.matmul(mat_a, mat_b)
+
 with tf.Session() as sess:
     print "Total: " + str(sess.run(total))
     print "Quotient: " + str(sess.run(quot))
+    print "Matrix Product: " + str(sess.run(mat_product))
